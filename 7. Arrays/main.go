48ce1aac20bfe11d4ef1187f75347b8c68d876c4
Vandhana-Mohan <mohan.vandhana@gmail.com>
2024-01-07 04:30:54 -0500

    arrays

--- 7. Arrays/main.go
+++ 7. Arrays/main.go
@@ -1,6 +1,30 @@
 package main
 import "fmt"
 
-func main(){
-	
+func main() {
+    myTutors := []string{"Kirsty", "Mishell", "Jose", "Neil"}
+    changeLastElement(myTutors, "Bobby")
+    fmt.Println(myTutors)
+    theirHours := [4]int{20,10,30,15}
+    theirHoursSlice := theirHours[:]
+    changeFirst(theirHoursSlice, 25)
+    fmt.Println(theirHoursSlice)
+    printFirstLastArray(theirHours)
+}
+
+func changeLastElement(slice [] string, newName string){
+  slice[len(slice) - 1] = newName
+  fmt.Println(slice)
+}
+
+func printFirstLastArray(array [4]int) {
+    fmt.Println("First", array[0])
+    fmt.Println("Last", array[3])
+}
+
+
+func changeFirst(slice []int, value int) {
+    if (len(slice) > 0) {
+        slice[0] = value
+    }
 }
