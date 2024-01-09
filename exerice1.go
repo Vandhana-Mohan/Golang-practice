fd561fee685cd91dc8567b20cfc854bc0df184d9
Vandhana-Mohan <mohan.vandhana@gmail.com>
2024-01-09 17:04:02 -0500

    practice udemy

--- exerice1.go
+++ exerice1.go
@@ -7,7 +7,7 @@ func main(){
 	fmt.Println("♥️") // print a emoji
 	fmt.Println(`hi im practicing udemy exercise 1 with emoji and string template literals`) // print raw string literal
 
-	var a int 
+	var a int
 	var b float64
 	var c complex64
 	var d string 
@@ -17,4 +17,8 @@ func main(){
 
 	add, bdd:= 23, "hi"
 	fmt.Println("a", a, "b", b, "c", c, "d", d, "e", e, "f", f, "g", g, add, bdd)
+
+	for i:= 1; i <= 10; i++{
+		fmt.Println(i)
+	}
 }
