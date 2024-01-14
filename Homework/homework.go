88b95dc2331cf41c439e67a5a0c81c4c6e0ac89a
Vandhana-Mohan <mohan.vandhana@gmail.com>
2024-01-13 23:03:10 -0500

    homework done

--- Homework/homework.go
+++ Homework/homework.go
@@ -11,11 +11,24 @@ The first argument is a string "This number is an odd number."
 The second argument is a number 11.
 "This number is an odd number", 11.
 
+Create a function that calculates the area of a rectangle.
+If the input is not a valid pair of numbers (length and width),
+output the message "Please enter valid dimensions." Return two arguments:
+The first argument is a string "The area of the rectangle is calculated."
+The second argument is the calculated area.
+Example output for valid dimensions: "The area of the rectangle is calculated", 30.
+
 */
 
 package main
 
 import "fmt"
+
+
+func calculateAreaRectangle(len, width int) (string, int){
+	area := len * width
+	return "The area of the rectangle is calculated : ", area
+}
 
 func checkNumberEvenOrOdd(num int) (string){
 	if(num % 2 == 0){
@@ -34,5 +47,20 @@ func main(){
 	} else {
 		result := checkNumberEvenOrOdd(num)
 		fmt.Println(result)
+	}
+
+	var length, width int
+
+	fmt.Print("Enter the length of the rectangle : ")
+	_, err1 := fmt.Scan(&length)
+
+	fmt.Print("Enter the width of the rectangle : ")
+	_,err2 := fmt.Scan(&width)
+
+	if(err1 != nil || err2 != nil){
+		fmt.Println("Please enter valid number.")
+	} else {
+		result, value := calculateAreaRectangle(length, width)
+		fmt.Println(result, value)
 	}
 }
