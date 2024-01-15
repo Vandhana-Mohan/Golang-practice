5657e595d744219f8d2f56ac3f67f94842a9c478
Vandhana-Mohan <mohan.vandhana@gmail.com>
2024-01-14 22:48:47 -0500

    practice pointer

--- 4. Pointers/main.go
+++ 4. Pointers/main.go
@@ -25,6 +25,17 @@ func main(){
 	
 	brainwash(&greeting)
 	fmt.Println("greeting is now:", greeting)
+
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 }
 
 // explaining pass by value
