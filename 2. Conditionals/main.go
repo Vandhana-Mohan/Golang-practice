f1b564fb254cdf5bbfbe5aa9f355d6bddd7d79b7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2024-01-14 22:47:59 -0500

    practice defer

--- 2. Conditionals/main.go
+++ 2. Conditionals/main.go
@@ -63,5 +63,13 @@ func main() {
   defer fmt.Println("world")
 
 	fmt.Println("hello")
+
+  fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
   
 }
